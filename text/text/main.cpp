3652ea9bed868748613d5773438bb54e0d059151
lichenxi200221@163.com <lichenxi20022@163.com>
2022-01-28 20:23:54 +0800

    first

---
+++ text/text/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  text
+//
+//  Created by 这个男人来自地球 on 2022/1/28.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
